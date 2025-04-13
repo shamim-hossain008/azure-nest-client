666edd838b5ec52052cc2cf6106571cb6516f5be
shamim-hossain008 <shamim.hossain3573@gmail.com>
2025-04-13 23:49:03 +0800

    added delete modal & room data show in a table

--- src/pages/Dashboard/Host/MyListings.jsx
+++ src/pages/Dashboard/Host/MyListings.jsx
@@ -2,13 +2,20 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { Helmet } from "react-helmet";
 import SpinnerLoader from "../../../Components/SpinnerLoader";
+import RoomDataRow from "../../../Components/TableRows/RoomDataRow";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const MyListings = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
-  const { data: rooms = [], isLoading } = useQuery({
+
+  // Fetch room data
+  const {
+    data: rooms = [],
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ["my-listings", user?.email],
     queryFn: async () => {
       const { data } = await axiosSecure.get(`/my-listings/${user?.email}`);
@@ -16,6 +23,11 @@ const MyListings = () => {
     },
   });
 
+  // Handle delete
+  const handleDelete = (id) => {
+    console.log(id);
+  };
+  // Handle Update
   if (isLoading) return <SpinnerLoader />;
   return (
     <div>
@@ -28,13 +40,13 @@ const MyListings = () => {
         <div className="container mx-auto px-4 sm:px-8">
           <div className="py-8">
             <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-              <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+              <div className="inline-block min-w-auto shadow rounded-lg overflow-hidden">
                 <table className="min-w-full leading-normal">
                   <thead>
                     <tr>
                       <th
                         scope="col"
-                        className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                        className="px-5 py-3 bg-white border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
                       >
                         Title
                       </th>
@@ -76,7 +88,17 @@ const MyListings = () => {
                       </th>
                     </tr>
                   </thead>
-                  <tbody>{/* Room row data */}</tbody>
+                  <tbody>
+                    {/* Room row data */}
+                    {rooms.map((room) => (
+                      <RoomDataRow
+                        key={room._id}
+                        room={room}
+                        refetch={refetch}
+                        handleDelete={handleDelete}
+                      />
+                    ))}
+                  </tbody>
                 </table>
               </div>
             </div>
