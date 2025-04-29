3c4b8105e2e6f2ec569f9f847f14a660aee99b8e
shamim-hossain008 <shamim.hossain3573@gmail.com>
2025-04-29 21:16:41 +0800

    added user Update Modal

---
+++ src/Components/TableRows/UserDataRow.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import UpdateUserModal from "../Modal/UpdateUserModal";
+
+const UserDataRow = ({ user, refetch }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  //   Modal handler
+  const modalHandler = () => {
+    console.log("user Role Updated");
+  };
+  return (
+    <tr>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">{user?.email}</p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">{user?.role}</p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        {user?.status ? (
+          <p
+            className={`${
+              user.status === "Verified" ? "text-green-500" : "text-yellow-500"
+            } whitespace-no-wrap`}
+          >
+            {user?.status}
+          </p>
+        ) : (
+          <p className="text-red-500 whitespace-no-wrap">Unavailable</p>
+        )}
+      </td>
+
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <button
+          onClick={() => setIsOpen(true)}
+          className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
+        >
+          <span
+            aria-hidden="true"
+            className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+          ></span>
+          <span className="relative">Update Role</span>
+        </button>
+        {/* Update User Modal */}
+        <UpdateUserModal
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          modalHandler={modalHandler}
+          user={user}
+        />
+      </td>
+    </tr>
+  );
+};
+
+export default UserDataRow;
--- src/pages/Dashboard/Admin/ManageUsers.jsx
+++ src/pages/Dashboard/Admin/ManageUsers.jsx
@@ -1,7 +1,85 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { Helmet } from "react-helmet";
+import SpinnerLoader from "../../../Components/SpinnerLoader";
+import UserDataRow from "../../../Components/TableRows/UserDataRow";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const ManageUsers = () => {
-  return <div>ManageUsers</div>;
+  const { user, loading } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  // fetch user dat
+  const {
+    data: users = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/users`);
+      return data;
+    },
+  });
+  console.log("users manage data", users);
+
+  if ((isLoading, loading)) return <SpinnerLoader />;
+  return (
+    <>
+      <div className="container mx-auto px-4 sm:px-8">
+        <Helmet>
+          <title>Manage Users</title>
+        </Helmet>
+        <div className="py-8">
+          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div className="inline-block min-w-auto shadow rounded-lg overflow-hidden">
+              <table className="min-w-full leading-normal">
+                <thead>
+                  <tr className="text-black font-bold bg-[#2A80B9]">
+                    <th
+                      scope="col"
+                      className="px-5 py-3  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                    >
+                      Email
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-5 py-3  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                    >
+                      Role
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-5 py-3  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                    >
+                      Status
+                    </th>
+
+                    <th
+                      scope="col"
+                      className="px-5 py-3  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                    >
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {users.map((user) => (
+                    <UserDataRow
+                      key={user?._id}
+                      user={user}
+                      refetch={refetch}
+                    />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ManageUsers;
